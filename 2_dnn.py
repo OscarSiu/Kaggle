c3baefe52d47ff02625cd7c54629c5ec183f53f7
Siu Cheuk Yin <68896149+OscarSiu@users.noreply.github.com>
2023-07-25 17:46:43 +0800

    Update 2_dnn.py

--- 2_dnn.py
+++ 2_dnn.py
@@ -1,7 +1,7 @@
 #Deep Neural Network
 
 import matplotlib.pyplot as plt
-#import tensorflow as tf
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
@@ -16,27 +16,22 @@ plt.rc('axes', labelweight='bold', labelsize='large',
 #Read CSV
 import pandas as pd
 
-concrete = pd.read_csv('../input/dl-course-data/concrete.csv')
-print(concrete.head())
-#concrete.shape()
+football_team = pd.read_csv('Football_teams.csv')
+print(football_team.head())
+#print(football_team.shape)
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 # Define a linear model
 model =  keras.Sequential([
-    layers.Dense(units=1, input_shape=[11]),
-    #Activation layer: 'relu', 'elu', 'selu', 'swish'
     #Hidden layers
-    layers.Dense(units=512, activation='relu', input_shape=[8]),
+    layers.Dense(units=512, activation='relu', input_shape=[9]), # input_shape = dimension of input
     layers.Dense(units=512, activation='relu'),
     layers.Dense(units=512, activation='relu'),
     
     #linear output layer
-    layers.Dense(units=1)
+    layers.Dense(units=1) #units = number of outputs
 ])
-
-w, b = model.weights
-print("Weights\n{}\n\nBias\n{}".format(w, b))
-
+#print(model.weights)
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 #Untrained Linear Model
 x = tf.linspace(-1.0, 1.0, 100)
@@ -53,14 +48,14 @@ w, b = model.weights # you could also use model.get_weights() here
 plt.title("Weight: {:0.2f}\nBias: {:0.2f}".format(w[0][0], b[0]))
 plt.show()
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-activation_layer = layers.Activation('swish')
+activation_layer = layers.Activation('swish')     #Activation layer: 'relu', 'elu', 'selu', 'swish'
 
-x = tf.linspace(-3.0, 3.0, 100)
+x = tf.linspace(-5.0, 5.0, 100)
 y = activation_layer(x) # once created, a layer is callable just like a function
 
 plt.figure(dpi=100)
 plt.plot(x, y)
-plt.xlim(-3, 3)
+plt.xlim(-5, 5)
 plt.xlabel("Input")
 plt.ylabel("Output")
 plt.show()
