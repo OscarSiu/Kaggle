e9148769e6569e7d4fcab37d7b42cc400ff3a596
Siu Cheuk Yin <68896149+OscarSiu@users.noreply.github.com>
2023-07-26 12:18:10 +0800

    Update 4_classification.py

--- 4_classification.py
+++ 4_classification.py
@@ -9,44 +9,55 @@ from IPython.display import display
 from tensorflow import keras
 from tensorflow.keras import layers
 
-ion = pd.read_csv('../input/dl-course-data/ion.csv', index_col=0)
-display(ion.head())
 
-df = ion.copy()
-df['Class'] = df['Class'].map({'good': 0, 'bad': 1})
+football_team = pd.read_csv('../input/football-teams-rankings-stats/Football teams.csv', usecols = ["Goals","Shots pg","Possession%","Pass%","AerialsWon", "Rating"])
 
-df_train = df.sample(frac=0.7, random_state=0)
-df_valid = df.drop(df_train.index)
+# Create training and validation splits
+df_train = football_team.sample(frac=0.7, random_state=0)
+df_valid = football_team.drop(df_train.index)
+display(df_train.head(5))
 
+# Scale to [0, 1]
 max_ = df_train.max(axis=0)
 min_ = df_train.min(axis=0)
-
 df_train = (df_train - min_) / (max_ - min_)
 df_valid = (df_valid - min_) / (max_ - min_)
-df_train.dropna(axis=1, inplace=True) # drop the empty feature in column 2
-df_valid.dropna(axis=1, inplace=True)
 
-X_train = df_train.drop('Class', axis=1)
-X_valid = df_valid.drop('Class', axis=1)
-y_train = df_train['Class']
-y_valid = df_valid['Class']
+# Split features and target
+X_train = df_train.drop('Rating', axis=1)
+X_valid = df_valid.drop('Rating', axis=1)
+y_train = df_train['Rating']
+y_valid = df_valid['Rating']
+print(X_train.shape)
 
+""""""""""""""""""""""""""""""""""""""""""""""""""""
+Dropout prevents overfitting
+rate defines percentage of the input units to shut off
+
+Batch Normalization helps correct training that is slow or unstable
+"""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 model = keras.Sequential([
-    layers.BatchNormalization(input_shape=input_shape),
-    layers.Dense(256, activation='relu'),
     layers.BatchNormalization(),
-    layers.Dropout(0.3),
-    layers.Dense(256, activation='relu'),
+    layers.Dense(16, activation='relu'),
+    #layers.Dropout(rate = 0.3),
     layers.BatchNormalization(),
-    layers.Dropout(0.3),   
-    layers.Dense(1, activation='sigmoid'),
+    layers.Dense(16, activation='relu'),
+    #layers.Dropout(0.3),
+    layers.BatchNormalization(),
+    
+    layers.Dense(1),
+    #layers.Dense(1, activation='sigmoid'),
 ])
 
 model.compile(
-    optimizer='adam',
-    loss='binary_crossentropy',
-    metrics=['binary_accuracy'],
+   # optimizer='adam',
+   # loss='binary_crossentropy',
+   # metrics=['binary_accuracy'],
+    optimizer='sgd',
+    loss='mae',
+    metrics=['mae'],
+
 )
 
 early_stopping = keras.callbacks.EarlyStopping(
@@ -58,18 +69,22 @@ early_stopping = keras.callbacks.EarlyStopping(
 history = model.fit(
     X_train, y_train,
     validation_data=(X_valid, y_valid),
-    batch_size=512,
-    epochs=200,
+    batch_size=256,
+    epochs=50,
     callbacks=[early_stopping],
-    verbose=0, # hide the output because we have so many epochs
+    #verbose=0, # hide the output because we have so many epochs
 )
 
 history_df = pd.DataFrame(history.history)
 # Start the plot at epoch 5
 history_df.loc[:, ['loss', 'val_loss']].plot()
-history_df.loc[:, ['binary_accuracy', 'val_binary_accuracy']].plot()
+#history_df.loc[:, ['binary_accuracy', 'val_binary_accuracy']].plot()
 
+"""
 print(("Best Validation Loss: {:0.4f}" +\
       "\nBest Validation Accuracy: {:0.4f}")\
       .format(history_df['val_loss'].min(), 
               history_df['val_binary_accuracy'].max()))
+"""
+
+print("Minimum validation loss: {}".format(history_df['val_loss'].min()))
